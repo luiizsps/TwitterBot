5d634cfe18b36294dcf5b5cf6e6083857328db73
luiizsps <103710492+luiizsps@users.noreply.github.com>
2022-06-29 22:03:10 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 import tweepy
 import time
 api = tweepy.Client(
-    consumer_key = '8hbV39T40Mzg6b1XTUOJKzHJk',
-    consumer_key = '7uEYxQhcYWxuCZxZGCMm5PsSLijr0lbPIEzubeJ2kgO4Er97bw',
-    access_token = '1251592701914595329-95AGglPoWXLAbnxsMvaIoosxp7jalS',
-    access_token_secret = 'ssNgEgvTFjZYQIpRftqEYeWgziMxaKjKkEEnKRHDgAfjV'
+    consumer_key = '',
+    consumer_key = '',
+    access_token = '',
+    access_token_secret = ''
 )
 
 api.
